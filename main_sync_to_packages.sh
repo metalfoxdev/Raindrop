83c079d7556c77b0234adcbe6b3c40c3e98afd65
metalfoxdev <92944520+metalfoxdev@users.noreply.github.com>
2023-04-03 19:21:25 +0100

    Correct python package folder

--- main_sync_to_packages.sh
+++ main_sync_to_packages.sh
@@ -1,3 +1,3 @@
 echo Syncing main to pypi package
-rsync -a --delete main.py packages/pypi/src/Raindrop_metalfoxdev/main.py
+rsync -a --delete main.py packages/pypi/src/Raindrop/main.py
 echo Sync Finished
